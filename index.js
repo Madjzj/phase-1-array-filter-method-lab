e34f8097e9b91d06831229c941386652562d86c4
JakobJohnson <madjzj@gmail.com>
2023-08-12 15:20:42 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const matched = [];
+    for (const driver of drivers){
+        if(driver.toLowerCase() == name.toLowerCase()){
+            matched.push(driver);
+        }
+    }
+    return matched;
+}
+function fuzzyMatch(drivers, name){
+    const matched = [];
+    const length = name.length
+    for (const driver of drivers){
+        if(driver.toLowerCase().slice(0,length) == name.toLowerCase()){
+            matched.push(driver);
+        }
+    }
+    return matched
+}
+function matchName(drivers, name){
+    const matched = [];
+    for (const driver of drivers){
+        if(driver.name.toLowerCase() == name.toLowerCase()){
+            matched.push(driver);
+        }
+    }
+    return matched
+}
